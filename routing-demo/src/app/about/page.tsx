6ef7de6d60760d784119b6fd893e84aea4a49c3f
Chandini-Kandula-tl <chandini@tilicho.in>
2024-11-05 22:22:57 +0530

    link component navigation - 19

--- routing-demo/src/app/about/page.tsx
+++ routing-demo/src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About Next js",
+};
+
 export default function about() {
   return <h1>About me</h1>;
 }
--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,10 +1,26 @@
+import Link from "next/link";
+
 export default function productList() {
+  const productId = 100;
+
   return (
     <>
-      <h1>Product list</h1>
-      <p>Product 1</p>
-      <p>Product 2</p>
-      <p>Product 3</p>
+      <Link href="/">Home</Link>
+      <h1>Product listing</h1>
+      <p>
+        <Link href="products/1">Product 1</Link>
+      </p>
+      <p>
+        <Link href="products/2">Product 2</Link>
+      </p>
+      <p>
+        <Link href="products/3" replace>
+          Product 3
+        </Link>
+      </p>
+      <p>
+        <Link href={`products/${productId}`}>Product {productId}</Link>
+      </p>
     </>
   );
 }
