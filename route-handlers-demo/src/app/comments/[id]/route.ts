2abb971bb2c3fabf2168f483cd4466d369fb776b
Tejaswini-Tilicho <tejaswini@tilicho.in>
2024-11-06 20:23:19 +0530

    Caching in Route handlers

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -13,39 +13,34 @@ import { comments } from "../data";
 
 export async function PATCH(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
+  const id = (await params).id;
   const body = await request.json();
   const { text } = body;
-  const index = comments.findIndex(
-    (comment) => comment.id === parseInt(params.id)
-  );
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
   comments[index].text = text;
   return Response.json(comments[index]);
 }
 
 export async function GET(
   _request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  console.log(params.id, "edrtfghujik");
-  if (parseInt(params.id) > comments.length) {
+  const id = (await params).id;
+  if (parseInt(id) > comments.length) {
     redirect("/comments");
   }
-  const comment = comments.find(
-    (comment) => comment.id === parseInt(params.id)
-  );
-  console.log(comment, "dcfrghuj", comments);
+  const comment = comments.find((comment) => comment.id === parseInt(id));
   return Response.json(comment);
 }
 
 export async function DELETE(
-  request: Request,
-  { params }: { params: { id: string } }
+  _request: Request,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const index = comments.findIndex(
-    (comment) => comment.id === parseInt(params.id)
-  );
+  const id = (await params).id;
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
   const deletedComment = comments[index];
   comments.splice(index, 1);
   return Response.json(deletedComment);
