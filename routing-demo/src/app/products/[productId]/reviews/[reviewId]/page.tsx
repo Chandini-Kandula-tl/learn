dcf3f64c34e1afc95ee6ba14f28233242bc3cec6
Chandini-Kandula-tl <chandini@tilicho.in>
2024-11-06 11:42:14 +0530

    Error handling - 24

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,19 @@
+"use client";
 import { notFound } from "next/navigation";
 
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
 export default function reviewDetail({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  const random = getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
+
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   }
