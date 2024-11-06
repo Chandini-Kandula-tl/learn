5a3a079c877ff7506adedbb5c042e1b2d10704cd
Chandini-Kandula-tl <chandini@tilicho.in>
2024-11-06 13:05:20 +0530

    unmatched routes - 29

--- routing-demo/src/app/complex-dashboard/@notifications/page.tsx
+++ routing-demo/src/app/complex-dashboard/@notifications/page.tsx
@@ -1,7 +1,13 @@
 import Card from "@/components/card";
+import Link from "next/link";
 
-const notifications = () => {
-  return <Card>Notifications</Card>;
-};
-
-export default notifications;
+export default function Notifications() {
+  return (
+    <Card>
+      <div>Notifications</div>
+      <div>
+        <Link href="/complex-dashboard/archived">Archived</Link>
+      </div>
+    </Card>
+  );
+}
