fed85c841c519bad36648ead3ebe323cb528b043
Tejaswini-Tilicho <tejaswini@tilicho.in>
2024-11-06 17:35:15 +0530

    Redirects in Route Handlers

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -1,14 +1,15 @@
+import { redirect } from "next/navigation";
 import { comments } from "../data";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
-  const comment = comments.find(
-    (comment) => comment.id === parseInt(params.id)
-  );
-  return Response.json(comment);
-}
+// export async function GET(
+//   request: Request,
+//   { params }: { params: { id: string } }
+// ) {
+//   const comment = comments.find(
+//     (comment) => comment.id === parseInt(params.id)
+//   );
+//   return Response.json(comment);
+// }
 
 export async function PATCH(
   request: Request,
@@ -21,6 +22,21 @@ export async function PATCH(
   );
   comments[index].text = text;
   return Response.json(comments[index]);
+}
+
+export async function GET(
+  _request: Request,
+  { params }: { params: { id: string } }
+) {
+  console.log(params.id, "edrtfghujik");
+  if (parseInt(params.id) > comments.length) {
+    redirect("/comments");
+  }
+  const comment = comments.find(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  console.log(comment, "dcfrghuj", comments);
+  return Response.json(comment);
 }
 
 export async function DELETE(
