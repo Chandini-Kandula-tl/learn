a1979553d9fa81071fbdb1e0e7de3abe333cdff8
Chandini-Kandula-tl <chandini@tilicho.in>
2024-11-06 17:21:33 +0530

    headers in route handler - 41

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,15 @@
-export async function GET() {
-  return new Response("Profile API data");
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const headersList = headers();
+  const requestHeaders = new Headers(request.headers);
+  console.log((await headersList).get("Authorization"));
+  console.log(requestHeaders.get("Authorization"));
+
+  return new Response("<h1>Profile API data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
