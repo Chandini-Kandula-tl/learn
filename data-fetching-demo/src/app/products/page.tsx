1dfa8a19d22320054b78da1d05db4370b805f2b2
Chandini-Kandula-tl <chandini@tilicho.in>
2024-11-07 15:57:22 +0530

    caching data - 66

---
+++ data-fetching-demo/src/app/products/page.tsx
@@ -0,0 +1,28 @@
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+};
+
+export default async function ProductsPage() {
+  const productsResponse = await fetch("http://localhost:3001/products", {
+    next: { revalidate: 10 },
+  });
+
+  const products = await productsResponse.json();
+  return (
+    <ul className="space-y-4 p-4">
+      {products.map((product: Product) => (
+        <li
+          key={product.id}
+          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+        >
+          <h2 className="text-xl font-semibold">{product.title}</h2>
+          <p>{product.description}</p>
+          <p className="text-lg font-medium">${product.price}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
