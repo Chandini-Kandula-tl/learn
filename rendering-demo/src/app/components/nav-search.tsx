960601da90fec131e943eea7fa1530acd69ce1d1
Tejaswini-Tilicho <tejaswini@tilicho.in>
2024-11-07 12:55:24 +0530

    Client component placement

---
+++ rendering-demo/src/app/components/nav-search.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+export const NavSearch = () => {
+  //   const [search, setSearch] = useState("");
+
+  console.log(`NavSearch rendered`);
+  return <div>Nav search input</div>;
+};
