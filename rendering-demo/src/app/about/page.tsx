7b5bac2c238200eb573be2222327719ceae0915a
Chandini-Kandula-tl <chandini@tilicho.in>
2024-11-07 11:23:11 +0530

    streaming - 54

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import { cookies } from "next/headers";
+// import { serverSideFunction } from "@/utils/server-utils";
 
 export default async function AboutPage() {
   const cookieStore = cookies();
   const theme = (await cookieStore).get("theme");
   console.log(theme);
+  // const result = serverSideFunction();
   return (
     <>
       <h1>About page {new Date().toLocaleTimeString()}</h1>
