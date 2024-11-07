e876cc9aedb3612f7df55dd58b7fdb3ff879d2e3
Chandini-Kandula-tl <chandini@tilicho.in>
2024-11-07 14:39:08 +0530

    client-only code - 59

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -1,53 +1,12 @@
 "use client";
 
-import { useTheme } from "@/components/theme-provider";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
+import { clientSideFunction } from "@/utilis/client-utils";
 
 export default function ClientRoutePage() {
-  const theme = useTheme();
+  const result = clientSideFunction();
   return (
     <>
-      <h1
-        style={{
-          color: theme.colors.secondary,
-        }}
-      >
-        Server Route
-      </h1>
-      <div className="image-slider-container">
-        <Slider>
-          <div>
-            <img src="https://picsum.photos/400/200" alt="placeholder" />
-          </div>
-          <div>
-            <img src="https://picsum.photos/400/300" alt="placeholder" />
-          </div>
-          <div>
-            <img src="https://picsum.photos/400/250" alt="placeholder" />
-          </div>
-          <div>
-            <img src="https://picsum.photos/400/350" alt="placeholder" />
-          </div>
-        </Slider>
-      </div>
+      <h1>client Route {result}</h1>
     </>
   );
 }
-
-// "use client";
-
-// import { serverSideFunction } from "@/utilis/server-utils";
-
-// export default function ClientRoutePage() {
-//   console.log("client method rendered");
-//   const result = serverSideFunction();
-
-//   return (
-//     <>
-//       <h1>client Route</h1>
-//       <p>{result}</p>
-//     </>
-//   );
-// }
--- rendering-demo/src/app/server-route/page.tsx
+++ rendering-demo/src/app/server-route/page.tsx
@@ -4,10 +4,14 @@ import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 export default function ServerRoutePage() {
   const result = serverSideFunction();
+  // const clientResult = clientSideFunction();
   return (
     <>
       <h1>Server Route</h1>
-      <p>{result}</p>
+      <p>
+        {result}
+        {/* {clientResult} */}
+      </p>
       <ImageSlider />
     </>
   );
