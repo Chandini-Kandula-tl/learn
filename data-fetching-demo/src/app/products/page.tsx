a44b8911c18fffc675a9c17fc900e3f1afdd6035
Chandini-Kandula-tl <chandini@tilicho.in>
2024-11-07 22:17:38 +0530

    time based data revalidation - 70
    not working

--- data-fetching-demo/src/app/products/page.tsx
+++ data-fetching-demo/src/app/products/page.tsx
@@ -1,4 +1,3 @@
-import { cookies } from "next/headers";
 // export const fetchCache = "default-cache";
 
 type Product = {
@@ -9,17 +8,21 @@ type Product = {
 };
 
 export default async function ProductsPage() {
-  const productsResponse = await fetch(
-    "http://localhost:3001/products"
-    //  {
-    // cache: "no-store",
-    //   }
-  );
-  const cookieStore = cookies();
-  (await cookieStore).get("theme");
-  const detailsResponse = await fetch("http://localhost:3001/products/1");
-  const details = await detailsResponse.json();
-  const products = await productsResponse.json();
+  const response = await fetch("http://localhost:3001/products", {
+    next: { revalidate: 10 },
+  });
+  const products = await response.json();
+  // const productsResponse = await fetch(
+  // "http://localhost:3001/products"
+  //  {
+  // cache: "no-store",
+  //   }
+  // );
+  // const cookieStore = cookies();
+  // (await cookieStore).get("theme");
+  // const detailsResponse = await fetch("http://localhost:3001/products/1");
+  // const details = await detailsResponse.json();
+  // const products = await productsResponse.json();
   return (
     <ul className="space-y-4 p-4">
       {products.map((product: Product) => (
@@ -30,7 +33,7 @@ export default async function ProductsPage() {
           <h2 className="text-xl font-semibold">{product.title}</h2>
           <p>{product.description}</p>
           <p className="text-lg font-medium">${product.price}</p>
-          <p>{details.title}</p>
+          {/* <p>{details.title}</p> */}
         </li>
       ))}
     </ul>
